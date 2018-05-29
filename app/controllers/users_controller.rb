207da252a63076ed50fe675635e3ed4db848f9b7
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-05-29 02:52:52 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
-    if [:user][:password] == [:user][:password_confirmation]
+    if @user.password == @user.password_confirmation
 
     byebug
   end

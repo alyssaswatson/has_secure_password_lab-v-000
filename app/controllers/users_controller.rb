a39dbda79c2f391a5b01aa2867ac5e586e64682c
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-05-29 02:52:06 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def create
+    if [:user][:password] == [:user][:password_confirmation]
+      
     byebug
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
